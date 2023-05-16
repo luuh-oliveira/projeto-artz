d1274b19793048643ce2b38015fdab89b8257ffa
luuh-oliveira <lu.oliveira0505@gmail.com>
2023-05-15 23:44:38 -0300

    Verificação (Login), botão de Logout e textos

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -1,34 +1,136 @@
+function carregarComentarios(idSecao) {
+
+    fetch("/usuarios/carregarComentarios", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            idSecaoServer: idSecao,
+        })
+    })
+        .then(function (resposta) {
+            if (resposta.ok) {
+                if (resposta.status == 204) {
+                    var feed = document.getElementById("div_comentarios");
+                    var mensagem = document.createElement("span");
+                    mensagem.innerHTML = "Nenhum resultado encontrado."
+                    feed.appendChild(mensagem);
+                    throw "Nenhum resultado encontrado!!";
+                }
+
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("div_comentarios");
+                    feed.innerHTML = "";
+                    for (let i = 0; i < resposta.length; i++) {
+                        var publicacao = resposta[i];
+
+                        // criando e manipulando elementos do HTML via JavaScript
+                        var divPublicacao = document.createElement("div");
+                        var usuarioNome = document.createElement("h4");
+                        var pComentario = document.createElement("p");
+
+                        usuarioNome.innerHTML = publicacao.nome;
+                        pComentario.innerHTML = publicacao.comentario;
+
+                        divPublicacao.className = "publicacao";
+                        usuarioNome.className = "publicacao-nome";
+                        pComentario.className = "publicacao-titulo";
+
+                        divPublicacao.appendChild(usuarioNome);
+                        divPublicacao.appendChild(pComentario);
+                        feed.appendChild(divPublicacao);
+                    }
+
+
+                });
+            } else {
+                throw ('Houve um erro na API!');
+            }
+        }).catch(function (resposta) {
+            console.error(resposta);
+        });
+
+
+
+
+}
+
+function comentar(idSecao) {
+
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var idUsuario = sessionStorage.ID_USUARIO;
+
+    if (email != null && nome != null) {
+
+        var comentario = input_comentario.value;
+
+        if (comentario == "") {
+            alert("Insira um comentário!");
+        } else {
+
+            fetch("/usuarios/comentar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    idSecaoServer: idSecao,
+                    idUsuarioServer: idUsuario,
+                    comentarioServer: comentario
+                })
+            }).then(function (resposta) {
+
+                console.log("resposta: ", resposta);
+
+                if (resposta.ok) {
+
+                    alert("Comentário postado com sucesso!");
+                    location.reload();
+
+                } else {
+                    throw ("Houve um erro ao comentar!");
+                }
+            }).catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+            });
+
+            return false;
+
+        }
+    } else {
+        window.location = "../login.html";
+        alert('Faça login para continuar!');
+
+    }
+
+}
+
+
+
 // sessão
 function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    // var b_usuario = document.getElementById("b_usuario");
-
     if (email != null && nome != null) {
         
         var botaoCadastro = document.getElementById("button_cadastro");
-        var divBotao = document.getElementById("div_botao");
+        var divBotao = document.getElementById("div_button");
         
         botaoCadastro.style.display = "none";
 
-        // window.alert(`Seja bem-vindo, ${nome}!`);
-        // b_usuario.innerHTML = nome;
+        divBotao.innerHTML += `<button id="button_logout" onclick="limparSessao()">Sair</button>`
 
-        // return true;
-        // finalizarAguardar();
-    } else {
-
-        window.location = "../login.html";
-        alert('Faça login para continuar!');
-    
-    }
+    } 
 }
 
 function limparSessao() {
-    // aguardar();
     sessionStorage.clear();
-    // finalizarAguardar();
-    window.location = "../login.html";
+    window.location = "../index.html";
+
 }
